33bf0b054c25f6b28501c0856c8ed80b4d6d17cb
Agnes Muita <agnes.muita@alternateltd.com>
2022-03-05 22:40:19 +0300

    Add project files.

---
+++ QuizWebAppWith.NET/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using QuizWebAppWith.NET.Models;
+
+namespace QuizWebAppWith.NET.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<QuizWebAppWith.NET.Models.Question> Question { get; set; }
+    }
+}
---
+++ QuizWebAppWith.NET/Models/Question.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizWebAppWith.NET.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public int QuizQuestion { get; set; }
+        public int QuizAnswer { get; set; }
+
+        public Question()
+        {
+
+        }
+    }
+}
